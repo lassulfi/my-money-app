69d1dbd322ad7a943d469d9737ab7dbefbe16b36
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-11-14 07:13:16 -0300

    Conectando o billingCycle ao redux e definindo a visibilidade das abas

--- frontend/src/common/tab/tabActions.js
+++ frontend/src/common/tab/tabActions.js
@@ -4,3 +4,12 @@ export function selectTab(tabId) {
         payload: tabId
     }
 }
+
+export function showTabs(...tabsId) {
+    const tabsToShow = {}
+    tabsId.forEach(e => tabsToShow[e] = true)
+    return {
+        type: 'TAB_DISPLAYED',
+        payload: tabsToShow
+    }
+}
