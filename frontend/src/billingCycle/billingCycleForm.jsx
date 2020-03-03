4d02a12017bc38f473a10ba32a0e19952bfeb0f0
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-03-02 22:38:36 -0300

    Criado componente simples creditList

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,7 +5,9 @@ import { reduxForm, Field } from 'redux-form'
 
 import { init } from './billingCycleActions'
 
-import labelAndInput from '../common/form/labelAndInput'
+import LabelAndInput from '../common/form/LabelAndInput'
+
+import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
 
@@ -15,12 +17,13 @@ class BillingCycleForm extends Component {
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component={labelAndInput} readOnly={readOnly}
+                    <Field name='name' component={LabelAndInput} readOnly={readOnly}
                         label='Nome' cols='12 4' placeholder='Informe o nome' />
-                    <Field name='month' component={labelAndInput} readOnly={readOnly} 
+                    <Field name='month' component={LabelAndInput} readOnly={readOnly} 
                         type='number' label='Mês' cols='12 4' placeholder='Informe o mês'/>
-                    <Field name='year' component={labelAndInput} readOnly={readOnly} 
+                    <Field name='year' component={LabelAndInput} readOnly={readOnly} 
                         type='number' label='Ano' cols='12 4' placeholder='Informe o ano'/>
+                    <CreditList cols='12 6' readOnly={readOnly}/>
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>
