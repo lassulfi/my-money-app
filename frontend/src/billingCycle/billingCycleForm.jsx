5c4f236df9a94674a7885d950e7bfe2f68b2c8a2
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-03-16 21:21:58 -0300

    Finalizado componente creditList

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { reduxForm, Field } from 'redux-form'
+import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import { init } from './billingCycleActions'
 
@@ -12,7 +12,7 @@ import CreditList from './creditList'
 class BillingCycleForm extends Component {
 
     render() {
-        const { handleSubmit, readOnly } = this.props
+        const { handleSubmit, readOnly, credits } = this.props
 
         return (
             <form role='form' onSubmit={handleSubmit}>
@@ -23,7 +23,7 @@ class BillingCycleForm extends Component {
                         type='number' label='Mês' cols='12 4' placeholder='Informe o mês'/>
                     <Field name='year' component={LabelAndInput} readOnly={readOnly} 
                         type='number' label='Ano' cols='12 4' placeholder='Informe o ano'/>
-                    <CreditList cols='12 6' readOnly={readOnly}/>
+                    <CreditList cols='12 6' list={credits} readOnly={readOnly}/>
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>
@@ -37,6 +37,8 @@ class BillingCycleForm extends Component {
     }
 }
 
-BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm) 
+BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+const selector = formValueSelector('billingCycleForm')
+const mapStateToProps = state => ({credits: selector(state, 'credits')})
 const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch)
-export default connect(null, mapDispatchToProps)(BillingCycleForm)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,20 +1,48 @@
 import React, { Component } from 'react'
-import { Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Field, arrayInsert, arrayRemove } from 'redux-form'
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
 
 class CreditList extends Component {
 
+    add(index, item = {}) {
+        if(!this.props.readOnly) {
+            this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+        }
+    }
+
+    remove(index) {
+        if(!this.props.readOnly && this.props.list.length > 1){
+            this.props.arrayRemove('billingCycleForm', 'credits', index)
+        }
+    }
+
     renderRows() {
-        return (
-            <tr>
-                <td><Field name='credits[0].name' component={Input} 
+        const list = this.props.list || []
+        return list.map((item, index) => (
+            <tr key={index}>
+                <td><Field name={`credits[${index}].name`} component={Input} 
                     placeholder='Informe o nome' readOnly={this.props.readOnly} /></td>
-                <td><Field name='credits[0].value' component={Input} 
+                <td><Field name={`credits[${index}].value`} component={Input} 
                     placeholder='Informe o valor' readOnly={this.props.readOnly} /></td>
-                <td></td>
+                <td>
+                    <button type='button' className='btn btn-success' 
+                        onClick={() => this.add(index + 1)}>
+                        <i className='fa fa-plus'></i>
+                    </button>
+                    <button type='button' className='btn btn-warning' 
+                        onClick={() => this.add(index + 1, item)}>
+                        <i className='fa fa-clone'></i>
+                    </button>
+                    <button type='button' className='btn btn-danger' 
+                        onClick={() => this.remove(index)}>
+                        <i className='fa fa-trash-o'></i>
+                    </button>
+                </td>
             </tr>
-        )
+        ))
     }
 
     render() {
@@ -27,7 +55,7 @@ class CreditList extends Component {
                             <tr>
                                 <th>Nome</th>
                                 <th>Valor</th>
-                                <th>Ações</th>
+                                <th className='table-actions'>Ações</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -40,4 +68,5 @@ class CreditList extends Component {
     }
 }
 
-export default CreditList
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert, arrayRemove }, dispatch)
+export default connect(null, mapDispatchToProps)(CreditList)
