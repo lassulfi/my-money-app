716085d0afb1ae8d6c9b3fe291b037b7312fe77e
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-03-23 21:27:24 -0300

    Adicionada autenticação com token JWT

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,29 @@
-const express = require('express');
+const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
 
+    /**
+     * Protected API with JWT Token
+     */
     //Define the base URL for all routes
-    const router = express.Router()
-    server.use('/api', router)    
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)    
+
+    protectedApi.use(auth)
 
     //Billing cycle routes
     const billingCycle = require('../api/billingCycle/billingCycleService') 
-    billingCycle.register(router, '/billingCycles')
+    billingCycle.register(protectedApi, '/billingCycles')
+
+    /**
+     * Public API
+     */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const authService = require('../api/user/authService')
+    openApi.post('/login', authService.login)
+    openApi.post('/signup', authService.signup)
+    openApi.post('/validateToken', authService.validateToken)
 }
